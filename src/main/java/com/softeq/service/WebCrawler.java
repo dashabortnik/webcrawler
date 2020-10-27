6ba0aaae492052597b9efe8f04bc4a005dcee810
dashabortnik <ghastly_smile91376428>
2020-10-27 10:37:58 +0300

    Fixed wrong links in output

--- src/main/java/com/softeq/service/WebCrawler.java
+++ src/main/java/com/softeq/service/WebCrawler.java
@@ -74,7 +74,7 @@ public class WebCrawler {
                 links.add(currentUrl);
                 int totalHitsNumber = crawlerExecutor.countWordMatches(searchTermsList);
                 List<Integer> hitsByWord = crawlerExecutor.getHitsByWord();
-                searchData.add(new SearchResult(seed, totalHitsNumber, hitsByWord));
+                searchData.add(new SearchResult(currentUrl.getUrl(), totalHitsNumber, hitsByWord));
             } else {
                 System.out.println("Either there are no other links to follow, or the depth of crawling was exceeded.");
                 System.exit(0);
